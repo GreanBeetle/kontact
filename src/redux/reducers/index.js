57659c5ddf6504a0db041b3a57de42b25f57ff15
John Rykken <john.rykken@gmail.com>
2020-08-11 13:30:33 -0700

    implement useEffect hook on KontactListScreen. utilize mapStateToProps and connect on KontactListScreen

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux'
 import getKontactsReducer from './getKontactsReducer'
 
-export default combineReducers({ getKontacts: getKontactsReducer })
+export default combineReducers({ 
+  isGetting: getKontactsReducer.isGetting,
+  errorMessage: getKontactsReducer.errorMessage,
+  kontacts: getKontactsReducer.kontacts 
+})
 
--- src/screens/KontactListScreen.js
+++ src/screens/KontactListScreen.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native'
 import { GLOBAL_STYLES as STYLES } from '../styles'
+import { connect } from 'react-redux'
+import { getKontacts } from '../redux/actions'
 
 const KontactListScreen = ({ navigation }) => {
+  
+  useEffect(() => {
+    console.log('component did mount')
+    // add getKontacts call here 
+  }, [])
+  
   return (
     <SafeAreaView style={STYLES.container}>
       <View style={STYLES.standard}>
@@ -24,4 +32,9 @@ const KontactListScreen = ({ navigation }) => {
   )
 }
 
-export default KontactListScreen
+function mapStateToProps(state) {
+  const { isGetting, errorMessage, kontacts } = state 
+  return { isGetting, errorMessage, kontacts }
+}
+
+export default connect(mapStateToProps, { getKontacts })(KontactListScreen)
